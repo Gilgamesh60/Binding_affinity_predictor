7a742bd63c312386f04402bf7a85bd04f21dc657
Shaunak <104096164+Gilgamesh60@users.noreply.github.com>
2023-06-22 12:06:53 +0530

    Update functions.py

--- src/functions.py
+++ src/functions.py
@@ -53,18 +53,43 @@ def structure_to_graph(structure):
 
 #This function is used to calculate covalent interactions
 def interactions_graph(ligand_file, protein_file):
-    protein =  MolFromPDBFile(protein_file,sanitize=False)
-    protein_graph = structure_to_graph(protein)
-    ligand = next(SDMolSupplier(ligand_file,sanitize=False))
-    ligand_graph = structure_to_graph(ligand)
+    
     node_features = np.concatenate([ligand_graph["node_feat"], protein_graph["node_feat"]], axis = 0)
     num_nodes = ligand_graph["num_nodes"] + protein_graph["num_nodes"]
-    edge_index = np.concatenate([ligand_graph["edge_index"], protein_graph["edge_index"] + ligand_graph["num_nodes"]], axis = 1)
+    
+    intermolecular_bonds, weight = [], []
+    for ligand_atom_id in range(ligand_graph["num_nodes"]):
+        for protein_atom_id in range(protein_graph["num_nodes"]):
+            ligand_atom_pos = ligand_graph["node_positions"][ligand_atom_id]
+            protein_atom_pos = protein_graph["node_positions"][protein_atom_id]
+            distance = sum((ligand_atom_pos - protein_atom_pos) ** 2)
+            if distance < 5:
+                intermolecular_bonds.append((ligand_atom_id, protein_atom_id + ligand_graph["num_nodes"]))
+                intermolecular_bonds.append((protein_atom_id + ligand_graph["num_nodes"], ligand_atom_id))
+                weight.append(distance)
+                weight.append(distance)
+
+    if len(intermolecular_bonds) > 0:
+        intermolecular_edge_index = np.array(intermolecular_bonds, dtype = np.int64).T
+        intermolecular_edge_weight = np.array(weight, dtype = np.float64).reshape(1, len(intermolecular_bonds))
+    else:
+        intermolecular_edge_index = np.empty((2, 0), dtype = np.int64)
+        intermolecular_edge_weight = np.empty((1, 0), dtype = np.float64)
+
+    num_covalent_bonds = ligand_graph["edge_index"].shape[1] + protein_graph["edge_index"].shape[1]
+    edge_index = np.concatenate([ligand_graph["edge_index"], protein_graph["edge_index"] + ligand_graph["num_nodes"],
+                                intermolecular_edge_index], axis = 1)
+    edge_weight = np.concatenate([np.array([[1] * ligand_graph["edge_index"].shape[1]]), np.array([[1] * protein_graph["edge_index"].shape[1]]),
+                                intermolecular_edge_weight], axis = 1).reshape(edge_index.shape[1])
     node_positions = np.concatenate([ligand_graph["node_positions"], protein_graph["node_positions"]], axis = 0)
 
-    graph = {"node_feat": node_features,
-            "num_nodes": num_nodes,
-            "edge_index": edge_index,
-            "node_positions": node_positions}
+    graph = {
+        "node_feat": node_features,
+        "num_nodes": num_nodes,
+        "num_covalent_bonds": num_covalent_bonds,
+        "edge_index": edge_index,
+        "edge_attr": edge_weight,
+        "node_positions": node_positions,
+    }
 
     return graph
